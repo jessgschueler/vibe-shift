d51760942ac86bb78e26e90b351db52ad317cf70
jessgschueler <jessicagrace.schueler@gmail.com>
2022-07-26 10:49:28 -0700

    Add DAG

---
+++ airflow-dag/dags/main.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import twint
+import nest_asyncio
+from datetime import date, timedelta
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from google.cloud import bigquery
+from config import database, table, search_term
+from airflow import DAG
+from airflow.decorators import dag, task
+
+
+def tweet_scrape(search_term):
+    nest_asyncio.apply()
+    c = twint.Config()
+    c.Lang = "en"
+    c.Since = str(date.today())
+    c.Search = [search_term]
+    c.Pandas = True
+    twint.run.Search(c)
+    Tweets_df = twint.storage.panda.Tweets_df
+    return Tweets_df
+
+
+def vader_scores(tweet):
+    """
+    Run VADER sentiment analysis and return  values
+    """
+    analyzer = SentimentIntensityAnalyzer()
+    vs = analyzer.polarity_scores(tweet)
+    return list(vs.values())
+
+
+def score_columns(Tweets_df):
+    """
+    Apply those values to the tweets_df and then seperate them into distinct columns
+    """
+    Tweets_df['Sentiment'] = Tweets_df['tweet'].map(vader_scores)
+    Tweets_df[['negative','neutral','positive', 'compound']] = Tweets_df['Sentiment'].tolist()
+    Tweets_df.drop(columns='Sentiment', inplace=True)
+
+def to_parquet(Tweets_df):
+    Tweets_df.to_parquet(f'data/{table}.parquet')
+
+def load_bq(dataframe, table):
+    client = bigquery.Client()
+    df = dataframe
+    job = client.load_table_from_dataframe(df, table)
+    job.result()
+
+def run():
+    Tweets_df = tweet_scrape(search_term)
+    score_columns(Tweets_df)
+    to_parquet(Tweets_df)
+    load_bq(Tweets_df,f'{database}{table}')
+
+run()
