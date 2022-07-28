37eaf9c71ef9aca9db7433eed693aa4dcbbe0701
jessgschueler <jessicagrace.schueler@gmail.com>
2022-07-28 11:30:38 -0700

    clean and comment main.py

--- airflow-dag/dags/main.py
+++ airflow-dag/dags/main.py
@@ -1,6 +1,5 @@
 from numpy import append
 import pandas as pd
-import pandas_gbq
 import os
 from datetime import date, timedelta
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
@@ -37,18 +36,11 @@ def score_columns(Tweets_df):
     Tweets_df = Tweets_df.set_index('id')
     Tweets_df.to_csv(f'/opt/airflow/dags/data/{table}_sentiment.csv')
 
-# def to_parquet(Tweets_df):
-#     Tweets_df.to_parquet(f'/opt/airflow/dags/data/{table}.parquet')
-
-# def load_bq(dataframe, table):
-#     client = bigquery.Client()
-#     # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/google_creds/goog_creds.json"
-#     df = dataframe
-#     job = client.load_table_from_dataframe(df, table)
-#     job.result()
-
 @task
 def load_to_gbq():
+    """
+    Load to GBQ
+    """
     client = bigquery.Client()
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/google_creds/goog_creds.json"
     sent_df = pd.read_csv(f'/opt/airflow/dags/data/{table}_sentiment.csv')
@@ -57,34 +49,25 @@ def load_to_gbq():
     job = client.load_table_from_dataframe(sent_df, destination)
     job.result()
 
-    # client = bigquery.Client()
-    # sent_df = pd.read_json(f'/opt/airflow/dags/data/{table}_sentiment.json', lines=True)
-    # schema = [{'name':'id', 'type': 'STRING'},
-    #     {'name':'date', 'type': 'DATETIME'},
-    #     {'name': 'content', 'type': 'STRING'},
-    #     {'name': 'lang', 'type': 'STRING'},
-    #     {'name': 'url', 'type': 'STRING'},
-    #     {'name': 'negative', 'type': 'FLOAT'},
-    #     {'name': 'neutral', 'type': 'FLOAT'},
-    #     {'name': 'positive', 'type': 'FLOAT'},
-    #     {'name': 'copmpund', 'type': 'FLOAT'}]
-    # sent_df.to_gbq(f'{dataset}.{table}', project_id=f'{project_id}', if_exists='append', table_schema=schema)
-
 @task
 def sentiment_analysis():
+    """
+    Runs our sentiment analysis functions
+    """
     Tweets_df = tweet_scrape()
     score_columns(Tweets_df)
 
 @dag(
-    schedule_interval='@once',
+    schedule_interval='@daily',
     start_date=datetime.utcnow(),
     catchup=False,
 )
 def vibe_shift():
-    today = str(date.today())
+    #set date for twitter search, will run at midnight and pull tweets from previous days
+    yesterday = str(date.today() - timedelta(days=1))
     get_tweets=BashOperator(
         task_id='get_tweets',
-        bash_command=f'snscrape --jsonl --max 200 twitter-search "{search_term} since:{today}" > /opt/airflow/dags/data/{table}.json')
+        bash_command=f'snscrape --jsonl --max 200 twitter-search "{search_term} since:{yesterday}" > /opt/airflow/dags/data/{table}.json')
     sentiment_analysis_task= sentiment_analysis()
     load_gbq_task=load_to_gbq()
     done=EmptyOperator(
